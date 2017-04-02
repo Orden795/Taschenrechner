208155909ce861aac7f69b369480acd2406ff2f0
Oleg <iah51ginter@gso-koeln.de>
2017-04-02 13:39:38 +0200

    Oberfläche Update

--- Taschenrechner/Taschenrechner/Form1.Designer.cs
+++ Taschenrechner/Taschenrechner/Form1.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.PiButton = new System.Windows.Forms.Button();
-            this.WurzelButton = new System.Windows.Forms.Button();
-            this.PotenzButton = new System.Windows.Forms.Button();
-            this.InvertierenButton = new System.Windows.Forms.Button();
-            this.KlammerRechtsButton = new System.Windows.Forms.Button();
-            this.KlammerLinksButton = new System.Windows.Forms.Button();
             this.CButton = new System.Windows.Forms.Button();
             this.CEButton = new System.Windows.Forms.Button();
             this.AdditionButton = new System.Windows.Forms.Button();
@@ -59,66 +53,12 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 23);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(218, 20);
+            this.textBox1.Size = new System.Drawing.Size(274, 20);
             this.textBox1.TabIndex = 0;
-            // 
-            // PiButton
-            // 
-            this.PiButton.Location = new System.Drawing.Point(125, 135);
-            this.PiButton.Name = "PiButton";
-            this.PiButton.Size = new System.Drawing.Size(50, 42);
-            this.PiButton.TabIndex = 48;
-            this.PiButton.Text = "π ";
-            this.PiButton.UseVisualStyleBackColor = true;
-            // 
-            // WurzelButton
-            // 
-            this.WurzelButton.Location = new System.Drawing.Point(69, 183);
-            this.WurzelButton.Name = "WurzelButton";
-            this.WurzelButton.Size = new System.Drawing.Size(50, 42);
-            this.WurzelButton.TabIndex = 47;
-            this.WurzelButton.Text = "√";
-            this.WurzelButton.UseVisualStyleBackColor = true;
-            // 
-            // PotenzButton
-            // 
-            this.PotenzButton.Location = new System.Drawing.Point(69, 231);
-            this.PotenzButton.Name = "PotenzButton";
-            this.PotenzButton.Size = new System.Drawing.Size(50, 42);
-            this.PotenzButton.TabIndex = 46;
-            this.PotenzButton.Text = "x²";
-            this.PotenzButton.UseVisualStyleBackColor = true;
-            // 
-            // InvertierenButton
-            // 
-            this.InvertierenButton.Location = new System.Drawing.Point(127, 327);
-            this.InvertierenButton.Name = "InvertierenButton";
-            this.InvertierenButton.Size = new System.Drawing.Size(48, 42);
-            this.InvertierenButton.TabIndex = 45;
-            this.InvertierenButton.Text = "±";
-            this.InvertierenButton.UseVisualStyleBackColor = true;
-            // 
-            // KlammerRechtsButton
-            // 
-            this.KlammerRechtsButton.Location = new System.Drawing.Point(237, 135);
-            this.KlammerRechtsButton.Name = "KlammerRechtsButton";
-            this.KlammerRechtsButton.Size = new System.Drawing.Size(50, 42);
-            this.KlammerRechtsButton.TabIndex = 44;
-            this.KlammerRechtsButton.Text = ")";
-            this.KlammerRechtsButton.UseVisualStyleBackColor = true;
-            // 
-            // KlammerLinksButton
-            // 
-            this.KlammerLinksButton.Location = new System.Drawing.Point(181, 135);
-            this.KlammerLinksButton.Name = "KlammerLinksButton";
-            this.KlammerLinksButton.Size = new System.Drawing.Size(50, 42);
-            this.KlammerLinksButton.TabIndex = 43;
-            this.KlammerLinksButton.Text = "(";
-            this.KlammerLinksButton.UseVisualStyleBackColor = true;
             // 
             // CButton
             // 
-            this.CButton.Location = new System.Drawing.Point(71, 327);
+            this.CButton.Location = new System.Drawing.Point(236, 107);
             this.CButton.Name = "CButton";
             this.CButton.Size = new System.Drawing.Size(50, 42);
             this.CButton.TabIndex = 42;
@@ -127,52 +67,56 @@
             // 
             // CEButton
             // 
-            this.CEButton.Location = new System.Drawing.Point(71, 279);
+            this.CEButton.Location = new System.Drawing.Point(236, 59);
             this.CEButton.Name = "CEButton";
-            this.CEButton.Size = new System.Drawing.Size(48, 42);
+            this.CEButton.Size = new System.Drawing.Size(50, 42);
             this.CEButton.TabIndex = 41;
             this.CEButton.Text = "CE";
             this.CEButton.UseVisualStyleBackColor = true;
             // 
             // AdditionButton
             // 
-            this.AdditionButton.Location = new System.Drawing.Point(293, 135);
+            this.AdditionButton.Location = new System.Drawing.Point(180, 59);
             this.AdditionButton.Name = "AdditionButton";
             this.AdditionButton.Size = new System.Drawing.Size(50, 42);
             this.AdditionButton.TabIndex = 40;
             this.AdditionButton.Text = "+";
             this.AdditionButton.UseVisualStyleBackColor = true;
+            this.AdditionButton.Click += new System.EventHandler(this.AdditionButton_Click);
             // 
             // SubtraktionButton
             // 
-            this.SubtraktionButton.Location = new System.Drawing.Point(293, 183);
+            this.SubtraktionButton.Location = new System.Drawing.Point(180, 107);
             this.SubtraktionButton.Name = "SubtraktionButton";
             this.SubtraktionButton.Size = new System.Drawing.Size(50, 42);
             this.SubtraktionButton.TabIndex = 39;
             this.SubtraktionButton.Text = "-";
             this.SubtraktionButton.UseVisualStyleBackColor = true;
+            this.SubtraktionButton.Click += new System.EventHandler(this.SubtraktionButton_Click);
             // 
             // MultiplikationButton
             // 
-            this.MultiplikationButton.Location = new System.Drawing.Point(293, 231);
+            this.MultiplikationButton.Location = new System.Drawing.Point(180, 155);
             this.MultiplikationButton.Name = "MultiplikationButton";
             this.MultiplikationButton.Size = new System.Drawing.Size(50, 42);
             this.MultiplikationButton.TabIndex = 38;
             this.MultiplikationButton.Text = "x";
             this.MultiplikationButton.UseVisualStyleBackColor = true;
+            this.MultiplikationButton.Click += new System.EventHandler(this.MultiplikationButton_Click);
             // 
             // DivisionButton
             // 
-            this.DivisionButton.Location = new System.Drawing.Point(293, 279);
+            this.DivisionButton.Location = new System.Drawing.Point(180, 203);
             this.DivisionButton.Name = "DivisionButton";
             this.DivisionButton.Size = new System.Drawing.Size(50, 42);
             this.DivisionButton.TabIndex = 37;
             this.DivisionButton.Text = "÷";
             this.DivisionButton.UseVisualStyleBackColor = true;
+            this.DivisionButton.Click += new System.EventHandler(this.DivisionButton_Click);
             // 
             // KommaButton
             // 
-            this.KommaButton.Location = new System.Drawing.Point(237, 325);
+            this.KommaButton.Location = new System.Drawing.Point(124, 201);
             this.KommaButton.Name = "KommaButton";
             this.KommaButton.Size = new System.Drawing.Size(50, 42);
             this.KommaButton.TabIndex = 36;
@@ -181,114 +125,118 @@
             // 
             // GleichheitszeichechenButton
             // 
-            this.GleichheitszeichechenButton.Location = new System.Drawing.Point(293, 325);
+            this.GleichheitszeichechenButton.Location = new System.Drawing.Point(236, 155);
             this.GleichheitszeichechenButton.Name = "GleichheitszeichechenButton";
-            this.GleichheitszeichechenButton.Size = new System.Drawing.Size(50, 42);
+            this.GleichheitszeichechenButton.Size = new System.Drawing.Size(50, 88);
             this.GleichheitszeichechenButton.TabIndex = 35;
             this.GleichheitszeichechenButton.Text = "=";
             this.GleichheitszeichechenButton.UseVisualStyleBackColor = true;
             // 
             // Zahl_9_Button
             // 
-            this.Zahl_9_Button.Location = new System.Drawing.Point(237, 183);
+            this.Zahl_9_Button.Location = new System.Drawing.Point(124, 59);
             this.Zahl_9_Button.Name = "Zahl_9_Button";
             this.Zahl_9_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_9_Button.TabIndex = 34;
             this.Zahl_9_Button.Text = "9";
             this.Zahl_9_Button.UseVisualStyleBackColor = true;
+            this.Zahl_9_Button.Click += new System.EventHandler(this.Zahl_9_Button_Click);
             // 
             // Zahl_8_Button
             // 
-            this.Zahl_8_Button.Location = new System.Drawing.Point(181, 183);
+            this.Zahl_8_Button.Location = new System.Drawing.Point(68, 59);
             this.Zahl_8_Button.Name = "Zahl_8_Button";
             this.Zahl_8_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_8_Button.TabIndex = 33;
             this.Zahl_8_Button.Text = "8";
             this.Zahl_8_Button.UseVisualStyleBackColor = true;
+            this.Zahl_8_Button.Click += new System.EventHandler(this.Zahl_8_Button_Click);
             // 
             // Zahl_7_Button
             // 
-            this.Zahl_7_Button.Location = new System.Drawing.Point(125, 183);
+            this.Zahl_7_Button.Location = new System.Drawing.Point(12, 59);
             this.Zahl_7_Button.Name = "Zahl_7_Button";
             this.Zahl_7_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_7_Button.TabIndex = 32;
             this.Zahl_7_Button.Text = "7";
             this.Zahl_7_Button.UseVisualStyleBackColor = true;
+            this.Zahl_7_Button.Click += new System.EventHandler(this.Zahl_7_Button_Click);
             // 
             // Zahl_6_Button
             // 
-            this.Zahl_6_Button.Location = new System.Drawing.Point(237, 231);
+            this.Zahl_6_Button.Location = new System.Drawing.Point(124, 107);
             this.Zahl_6_Button.Name = "Zahl_6_Button";
             this.Zahl_6_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_6_Button.TabIndex = 31;
             this.Zahl_6_Button.Text = "6";
             this.Zahl_6_Button.UseVisualStyleBackColor = true;
+            this.Zahl_6_Button.Click += new System.EventHandler(this.Zahl_6_Button_Click);
             // 
             // Zahl_5_Button
             // 
-            this.Zahl_5_Button.Location = new System.Drawing.Point(181, 231);
+            this.Zahl_5_Button.Location = new System.Drawing.Point(68, 107);
             this.Zahl_5_Button.Name = "Zahl_5_Button";
             this.Zahl_5_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_5_Button.TabIndex = 30;
             this.Zahl_5_Button.Text = "5";
             this.Zahl_5_Button.UseVisualStyleBackColor = true;
+            this.Zahl_5_Button.Click += new System.EventHandler(this.Zahl_5_Button_Click);
             // 
             // Zahl_4_Button
             // 
-            this.Zahl_4_Button.Location = new System.Drawing.Point(125, 231);
+            this.Zahl_4_Button.Location = new System.Drawing.Point(12, 107);
             this.Zahl_4_Button.Name = "Zahl_4_Button";
             this.Zahl_4_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_4_Button.TabIndex = 29;
             this.Zahl_4_Button.Text = "4";
             this.Zahl_4_Button.UseVisualStyleBackColor = true;
+            this.Zahl_4_Button.Click += new System.EventHandler(this.Zahl_4_Button_Click);
             // 
             // Zahl_3_Button
             // 
-            this.Zahl_3_Button.Location = new System.Drawing.Point(237, 279);
+            this.Zahl_3_Button.Location = new System.Drawing.Point(124, 155);
             this.Zahl_3_Button.Name = "Zahl_3_Button";
             this.Zahl_3_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_3_Button.TabIndex = 28;
             this.Zahl_3_Button.Text = "3";
             this.Zahl_3_Button.UseVisualStyleBackColor = true;
+            this.Zahl_3_Button.Click += new System.EventHandler(this.Zahl_3_Button_Click);
             // 
             // Zahl_2_Button
             // 
-            this.Zahl_2_Button.Location = new System.Drawing.Point(181, 279);
+            this.Zahl_2_Button.Location = new System.Drawing.Point(68, 155);
             this.Zahl_2_Button.Name = "Zahl_2_Button";
             this.Zahl_2_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_2_Button.TabIndex = 27;
             this.Zahl_2_Button.Text = "2";
             this.Zahl_2_Button.UseVisualStyleBackColor = true;
+            this.Zahl_2_Button.Click += new System.EventHandler(this.Zahl_2_Button_Click);
             // 
             // Zahl_1_Button
             // 
-            this.Zahl_1_Button.Location = new System.Drawing.Point(125, 279);
+            this.Zahl_1_Button.Location = new System.Drawing.Point(12, 155);
             this.Zahl_1_Button.Name = "Zahl_1_Button";
             this.Zahl_1_Button.Size = new System.Drawing.Size(50, 42);
             this.Zahl_1_Button.TabIndex = 26;
             this.Zahl_1_Button.Text = "1";
             this.Zahl_1_Button.UseVisualStyleBackColor = true;
+            this.Zahl_1_Button.Click += new System.EventHandler(this.Zahl_1_Button_Click);
             // 
             // Zahl_0_Button
             // 
-            this.Zahl_0_Button.Location = new System.Drawing.Point(181, 327);
+            this.Zahl_0_Button.Location = new System.Drawing.Point(12, 203);
             this.Zahl_0_Button.Name = "Zahl_0_Button";
-            this.Zahl_0_Button.Size = new System.Drawing.Size(50, 42);
+            this.Zahl_0_Button.Size = new System.Drawing.Size(106, 42);
             this.Zahl_0_Button.TabIndex = 25;
             this.Zahl_0_Button.Text = "0";
             this.Zahl_0_Button.UseVisualStyleBackColor = true;
+            this.Zahl_0_Button.Click += new System.EventHandler(this.Zahl_0_Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 505);
-            this.Controls.Add(this.PiButton);
-            this.Controls.Add(this.WurzelButton);
-            this.Controls.Add(this.PotenzButton);
-            this.Controls.Add(this.InvertierenButton);
-            this.Controls.Add(this.KlammerRechtsButton);
-            this.Controls.Add(this.KlammerLinksButton);
+            this.ClientSize = new System.Drawing.Size(301, 255);
             this.Controls.Add(this.CButton);
             this.Controls.Add(this.CEButton);
             this.Controls.Add(this.AdditionButton);
@@ -318,12 +266,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button PiButton;
-        private System.Windows.Forms.Button WurzelButton;
-        private System.Windows.Forms.Button PotenzButton;
-        private System.Windows.Forms.Button InvertierenButton;
-        private System.Windows.Forms.Button KlammerRechtsButton;
-        private System.Windows.Forms.Button KlammerLinksButton;
         private System.Windows.Forms.Button CButton;
         private System.Windows.Forms.Button CEButton;
         private System.Windows.Forms.Button AdditionButton;
--- Taschenrechner/Taschenrechner/Form1.cs
+++ Taschenrechner/Taschenrechner/Form1.cs
@@ -16,10 +16,74 @@ namespace Taschenrechner
         {
             InitializeComponent();
         }
+        double Zahl1;
+        double Zahl2;
 
+        #region Zahleneingaben
         private void Zahl_0_Button_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + "0";
         }
+        private void Zahl_1_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+        }
+        private void Zahl_2_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+        private void Zahl_3_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+        private void Zahl_4_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+        private void Zahl_5_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+        private void Zahl_6_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+        private void Zahl_7_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+        }
+        private void Zahl_8_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+        private void Zahl_9_Button_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        #endregion
+
+        #region Operationen
+        private void AdditionButton_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToDouble(textBox1.Text);
+
+        }
+
+        private void SubtraktionButton_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToDouble(textBox1.Text);
+        }
+
+        private void MultiplikationButton_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToDouble(textBox1.Text);
+        }
+
+        private void DivisionButton_Click(object sender, EventArgs e)
+        {
+            Zahl1 = Convert.ToDouble(textBox1.Text);
+        }
+        #endregion
     }
 }
